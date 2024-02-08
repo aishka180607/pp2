60a8b75c62050445d3df72817f066719e4e9109e
Aitkali Aisha Oralbekkyzy <aitkaliaishaoralbekkyzy@MacBook-Pro-Aitkali.local>
2024-02-08 23:29:52 +0600

    lab3

---
+++ Lab3/inher.py
@@ -0,0 +1,27 @@
+#ex1
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+x = Person("John", "Doe")
+x.printname()
+class Student(Person):
+  pass
+
+#ex2
+class Person:
+  def __init__(self, fname):
+    self.firstname = fname
+
+  def printname(self):
+    print(self.firstname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike")
+x.printname()
