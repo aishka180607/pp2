0dd8fa29b8fd0adfd78705c50a93623267d49a2f
Aitkali Aisha Oralbekkyzy <aitkaliaishaoralbekkyzy@MacBook-Pro-Aitkali.local>
2024-02-09 00:13:54 +0600

    lab3

---
+++ Lab3/func1/rev.py
@@ -0,0 +1,10 @@
+def rev(sentence):
+    words = sentence.split()
+    reversed_sentence = ' '.join(reversed(words))
+    return reversed_sentence
+
+user_input = input("Enter a sentence: ")
+
+
+result = rev(user_input)
+print(result)
