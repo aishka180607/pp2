feb779bd2811886f03dcd5e779469dc2c0ce0a9b
Aitkali Aisha Oralbekkyzy <aitkaliaishaoralbekkyzy@MacBook-Pro-Aitkali.local>
2024-03-09 14:31:08 +0500

    Lab5

--- Lab5/1.ex.py
+++ Lab5/1.ex.py
@@ -10,3 +10,18 @@ def match_pat(s):
 test_str = ['a', 'ab', 'abb', 'abbbb', 'b', 'bbb']
 for test in test_str:
     print(f"{test}: {match_pat(test)}")
+
+import re
+
+# Открываем файл на чтение
+with open("row.txt", "r") as file:
+    # Читаем строки файла
+    lines = file.readlines()
+
+    # Используем регулярное выражение для поиска строк с "мл" или "фл"
+    pattern = re.compile(r'\b\w*(мл|фл)\w*\b')
+
+    # Выводим найденные строки
+    for line in lines:
+        if pattern.search(line):
+            print(line.strip())
